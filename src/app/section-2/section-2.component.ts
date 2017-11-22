7e6aec30e97417c48899178995cba30c03b15847
byshako <byshako@gmail.com>
2017-11-22 15:15:11 +0400

    directives

--- src/app/section-2/section-2.component.ts
+++ src/app/section-2/section-2.component.ts
@@ -9,11 +9,14 @@ export class Section2Component implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'Server 1';
+  serverCreated = false;
+
   constructor() { }
 
   ngOnInit() {
   }
   onCreateServer(){
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server Created! Name is ' + this.serverName;
   }
   onUpdateServerName(event: any){
